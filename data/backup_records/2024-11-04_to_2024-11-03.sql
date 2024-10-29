5811e832838360221d8dcb6700c1f51850f4f0d6
itsGovi <govardhanyadavsangi@gmail.com>
2024-10-29 11:30:32 +0530

    testing backinup records

---
+++ data/backup_records/2024-11-04_to_2024-11-03.sql
@@ -0,0 +1,14 @@
+-- Incremental Backup
+-- Period: 2024-11-04 to 2024-11-03
+-- Generated: 2024-10-29 11:28:54
+-- Only includes changes since: 1970-01-01 00:00:00
+
+BEGIN;
+
+-- Modified records in departments
+-- Modified records in positions
+-- Modified records in employees
+-- Modified records in skills
+-- Modified records in employee_skills
+-- Modified records in leave_requests
+COMMIT;
