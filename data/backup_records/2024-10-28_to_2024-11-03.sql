8dd65d5fcbfa30466906df937fe41b32582cdcf2
itsGovi <govardhanyadavsangi@gmail.com>
2024-10-30 16:31:42 +0530

    silly mistakes in database.py

--- data/backup_records/2024-10-28_to_2024-11-03.sql
+++ data/backup_records/2024-10-28_to_2024-11-03.sql
@@ -60,3 +60,10 @@ COMMIT;
 -- Modified records in employee_skills on 2024-10-30
 -- Modified records in leave_requests on 2024-10-30
 COMMIT;
+-- Modified records in departments on 2024-10-30
+-- Modified records in positions on 2024-10-30
+-- Modified records in employees on 2024-10-30
+-- Modified records in skills on 2024-10-30
+-- Modified records in employee_skills on 2024-10-30
+-- Modified records in leave_requests on 2024-10-30
+COMMIT;
