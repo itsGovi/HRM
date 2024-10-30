31d5b6c6c29cd97cf944a3447fe396f1a49f695f
itsGovi <govardhanyadavsangi@gmail.com>
2024-10-30 16:03:18 +0530

    indexing mistakes in the integration setup folder

--- data/backup_records/2024-10-28_to_2024-11-03.sql
+++ data/backup_records/2024-10-28_to_2024-11-03.sql
@@ -50,3 +50,13 @@ COMMIT;
 -- Modified records in employee_skills on 2024-10-29
 -- Modified records in leave_requests on 2024-10-29
 COMMIT;
+
+
+### Changes for 2024-10-30 ###
+-- Modified records in departments on 2024-10-30
+-- Modified records in positions on 2024-10-30
+-- Modified records in employees on 2024-10-30
+-- Modified records in skills on 2024-10-30
+-- Modified records in employee_skills on 2024-10-30
+-- Modified records in leave_requests on 2024-10-30
+COMMIT;
