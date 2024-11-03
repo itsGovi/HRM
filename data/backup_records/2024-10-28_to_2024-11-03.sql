faf9616b5e56645f53966f36c17563cd0b4f03e0
itsGovi <govardhanyadavsangi@gmail.com>
2024-11-03 15:36:06 +0530

    just changed the file name to show seriousness of readme.md and sound cool

--- data/backup_records/2024-10-28_to_2024-11-03.sql
+++ data/backup_records/2024-10-28_to_2024-11-03.sql
@@ -132,3 +132,10 @@ COMMIT;
 -- Modified records in employee_skills on 2024-11-03
 -- Modified records in leave_requests on 2024-11-03
 COMMIT;
+-- Modified records in departments on 2024-11-03
+-- Modified records in positions on 2024-11-03
+-- Modified records in employees on 2024-11-03
+-- Modified records in skills on 2024-11-03
+-- Modified records in employee_skills on 2024-11-03
+-- Modified records in leave_requests on 2024-11-03
+COMMIT;
